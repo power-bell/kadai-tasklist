8664ca8d307c7a5c1ff44b8a4a8b325628c2ce36
kobayashi <w.d.kobayashi@gmail.com>
2020-12-14 12:07:31 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
   before_action :set_task,only:[:show,:edit,:update,:destroy]
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
-    @tasks=Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -12,7 +16,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task=Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success]="タスクが正常に作成されました"
@@ -52,6 +56,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content,:status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
